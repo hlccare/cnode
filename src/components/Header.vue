d4097607602f690da804a57d7dd93ea67e9ce479
hlccare <280108904@qq.com>
2021-05-08 09:46:33 +0800

    init

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="header">
+    <img src="@/assets/img/cnodejs_light.svg" alt="" />
+    <ul>
+      <li><a href="#">首页</a></li>
+      <li><a href="#">新手</a></li>
+      <li><a href="#">API</a></li>
+      <li><a href="#">关于</a></li>
+      <li><a href="#">登录</a></li>
+      <li><a href="#">注册</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,8 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <div>home</div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
+import { Component, Vue } from "vue-property-decorator";
 export default class Home extends Vue {}
 </script>
